509366507a86a8864b94d32c2502b71d6a7815ae
FilipeSouza <filipe.souza1602@gmail.com>
2022-03-03 23:56:14 -0300

    feat: configure app.module adding module and service of http client and postagens component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostagensComponent } from './components/postagens/postagens.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClientService } from 'src/infra/http-client';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostagensComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [HttpClientService, HttpClient],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
