3dd1787dd39e03c5c5deb2afcad320da0ccebdb3
FilipeSouza <filipe.souza1602@gmail.com>
2022-03-03 23:57:02 -0300

    feat: create postangens component

---
+++ src/app/components/postagens/postagens.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-postagens',
+  templateUrl: './postagens.component.html',
+  styleUrls: ['./postagens.component.scss']
+})
+export class PostagensComponent implements OnInit {
+
+  constructor() { }
+
+  async ngOnInit() {
+
+  }
+
+}
