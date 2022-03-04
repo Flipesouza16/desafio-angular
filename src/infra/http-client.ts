9f9773df6b05419b8ecde3e2fb483b060f718d83
FilipeSouza <filipe.souza1602@gmail.com>
2022-03-03 23:40:25 -0300

    feat: create infra with http client service

---
+++ src/infra/http-client.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { HttpResponse } from 'src/data/protocols/http/http-response';
+import { HttpGetClient, HttpParams } from 'src/data/protocols/http/http-protocol-client';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpClientService implements HttpGetClient<any, any> {
+
+  private httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  }
+
+  constructor(private http: HttpClient) { }
+
+  get(params: HttpParams<any>): Promise<HttpResponse<any>> {
+    return new Promise(resolve => {
+      this.http.get(params.url).subscribe((response: any) => {
+        resolve({
+          body: response
+        })
+      });
+    })
+  }
+}
