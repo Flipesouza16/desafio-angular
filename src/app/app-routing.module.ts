ba6bc432c6ce40a931f57d3b1c0d6fca48ae0f37
FilipeSouza <filipe.souza1602@gmail.com>
2022-03-03 23:57:29 -0300

    feat: set postagens to default route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PostagensComponent } from './components/postagens/postagens.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'postagens', component: PostagensComponent },
+  { path: '**', redirectTo: 'postagens' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
