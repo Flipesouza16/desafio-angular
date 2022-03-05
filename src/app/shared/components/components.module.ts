28eaf9ea50409e5e29a0ce6f4f1b6b4c731214b1
FilipeSouza <filipe.souza1602@gmail.com>
2022-03-05 20:24:59 -0300

    feat: make the header responsive and add MatMenu

--- src/app/shared/components/components.module.ts
+++ src/app/shared/components/components.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
 import { HeaderComponent } from "./header/header.component";
+import { ListItemsComponent } from './list-items/list-items.component';
 import { MatIconModule, MatListModule, MatToolbarModule } from "@angular/material";
 import { MatButtonModule } from "@angular/material/button";
 import { MatInputModule } from "@angular/material/input";
-import { ListItemsComponent } from './list-items/list-items.component';
-import { CommonModule } from "@angular/common";
+import { MatMenuModule } from '@angular/material/menu';
 
 @NgModule({
   declarations: [HeaderComponent, ListItemsComponent],
@@ -14,6 +15,7 @@ import { CommonModule } from "@angular/common";
     MatIconModule,
     MatButtonModule,
     MatInputModule,
+    MatMenuModule,
     MatListModule
   ],
   exports: [HeaderComponent, ListItemsComponent],
--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { ListPagesModel, ListTypePages } from '../../interfaces';
+import { Component, HostListener } from '@angular/core';
+import { ListPagesModel, ListTypePages, PageModel } from '../../interfaces';
 import { listPages } from '../../utils';
 
 @Component({
@@ -11,9 +11,30 @@ export class HeaderComponent {
   public listTypePages = ListTypePages;
   public titlePage = 'Postagens';
   public pages: ListPagesModel = listPages;
+  public screenWidth = document.documentElement.clientWidth;
+
+  get getArrayPages() {
+    const listPageKeys = Object.keys(this.pages);
+    let arrayPages: PageModel[] = [];
+
+    listPageKeys.forEach(key => {
+      arrayPages.push(this.pages[key]);
+    })
+
+    return arrayPages;
+  }
 
   selectPage(page: string): void {
     this.titlePage = this.pages[page].name;
   }
 
+  get isScreenSmall() {
+    if(this.screenWidth < 450) return true;
+    else return false;
+  }
+
+  @HostListener("window:resize", [])
+  private onResize() {
+    this.screenWidth = document.documentElement.clientWidth;
+  }
 }
