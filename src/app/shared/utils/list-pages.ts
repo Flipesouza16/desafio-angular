be6efa7d1e87f01b841a9901c7f1cc2c76589cb3
FilipeSouza <filipe.souza1602@gmail.com>
2022-03-05 20:21:41 -0300

    refactor: change "to-dos" to "todos"

--- src/app/shared/utils/list-pages.ts
+++ src/app/shared/utils/list-pages.ts
@@ -3,5 +3,5 @@ import { ListPagesModel } from "../interfaces";
 export const listPages: ListPagesModel = {
   postagens: { name: 'Postagens', slug: 'postagens' },
   albuns: { name: 'Albuns', slug: 'albuns' },
-  todos: { name: 'TO-DOs', slug: 'to-dos' },
+  todos: { name: 'TO-DOs', slug: 'todos' },
 }
