8c80d68e0916f358e7e070152007f13c37d6ca17
FilipeSouza <filipe.souza1602@gmail.com>
2022-03-05 17:18:51 -0300

    feat: add list of pages

---
+++ src/app/shared/utils/listPages.ts
@@ -0,0 +1,7 @@
+import { ListPagesModel } from "../interfaces/pagesModel";
+
+export const listPages: ListPagesModel = {
+  postagens: { name: 'Postagens', slug: 'postagens' },
+  albuns: { name: 'Albuns', slug: 'albuns' },
+  todos: { name: 'TO-DOs', slug: 'to-dos' },
+}
