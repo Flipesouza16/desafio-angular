40f1585cf3684b987bf9681499e12455e9c79092
FilipeSouza <filipe.souza1602@gmail.com>
2022-03-06 14:38:25 -0300

    feat: add reject in http-client-service

--- src/infra/http-client-service.ts
+++ src/infra/http-client-service.ts
@@ -1,30 +1,29 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
-import {
-  HttpGetClient,
-  HttpParams,
-  HttpResponse
-} from '@/data/protocols/http';
-import { environment } from '@/environments/environment';
+import { HttpGetClient, HttpParams, HttpResponse } from "@/data/protocols/http";
+import { environment } from "@/environments/environment";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class HttpClientService implements HttpGetClient<any, any> {
-
   private httpOptions = {
-    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
-  }
+    headers: new HttpHeaders({ "Content-Type": "application/json" }),
+  };
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   get(params: HttpParams<any>): Promise<HttpResponse<any>> {
-    return new Promise(resolve => {
-      this.http.get(`${environment.BASE_URL}/${params.url}`).subscribe((response: any) => {
-        resolve({
-          body: response
-        })
-      });
-    })
+    return new Promise((resolve, reject) => {
+      this.http
+        .get(`${environment.BASE_URL}/${params.url}`)
+        .subscribe((response: any) => {
+          resolve({
+            body: response,
+          });
+        }, (error) => {
+          reject(error);
+        });
+    });
   }
 }
